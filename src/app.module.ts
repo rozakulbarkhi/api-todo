01b1d828b4d798bb00e7af16d55bced2dac18b23
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-07-29 17:39:18 +0700

    auth integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    AuthModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
-  register() {
-    return { msg: 'register route' };
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      delete user.password;
+
+      return user;
+    } catch (error) {
+      throw new ForbiddenException('Email already taken');
+    }
   }
 
-  login() {
-    return { msg: 'login route' };
+  async login(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Username or password was wrong');
+    }
+
+    const comparePassword = await argon.verify(user.password, dto.password);
+
+    if (!comparePassword) {
+      throw new ForbiddenException('Username or password was wrong');
+    }
+
+    delete user.password;
+
+    return user;
   }
 }
